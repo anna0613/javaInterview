2b9aa320be4d2dd0902f09b71f6f225cad354cee
Administrator <123@qq.com>
2016-12-09 22:21:48 +0800

    节点删除

--- qingsongxueAlgorithm/src/me/algorithm/ch02/Link.java
+++ qingsongxueAlgorithm/src/me/algorithm/ch02/Link.java
@@ -37,7 +37,7 @@ public class Link {
     }
 
     /**
-     * 在index下标之后插入
+     * 在index下标之后插入 (index从0开始)
      * @param data
      * @param index
      */
@@ -75,6 +75,48 @@ public class Link {
         }
         return tmp;
     }
+    public void removeFirst(){
+        if(this.size == 0){
+            throw new IndexOutOfBoundsException("链表没有元素");
+        }else {
+            first = first.getNext();
+            this.size--;
+        }
+    }
+    public void removeLast(){
+        if(this.size == 0){
+            throw new IndexOutOfBoundsException("链表没有元素");
+        }else if(this.size == 1){
+            first = null;
+            last = null;
+            this.size--;
+        }else {
+            Node node = get(this.size - 2);
+            last = node;
+            node.setNext(null);
+            this.size--;
+        }
+    }
+
+    //index从0开始
+    public void remove(int index){
+        if(index < this.size){
+            if(index == 0){
+                removeFirst();
+            }else if(index + 1 == size){
+                removeLast();
+            }else{
+                Node tmp = get(index - 2);// 当前节点的前一个节点
+                Node next = tmp.getNext();//当前节点
+                tmp.setNext(next.getNext());
+                next = null;//空间回收
+                this.size--;
+            }
+        }else{
+            throw new IndexOutOfBoundsException("链表没那么长");
+        }
+    }
+
     public int size(){
         return  this.size;
     }
@@ -91,9 +133,13 @@ public class Link {
         link.addLast(4);
         link.addLast(8);
         link.addFirst(6);
-        link.addFirst(3);
+//        link.addFirst(3);
+//        link.add(7,2);
+//        link.removeFirst();
+//        link.removeLast();
+        link.remove(1);
 //        link.add(88,2);
         link.printAll();
-        System.out.println(link.size());
+//        System.out.println(link.size());
     }
 }
